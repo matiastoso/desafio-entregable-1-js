63eda99e1fda858b49fa6180659de2cfcea9c273
Matias <matiastoso.developer@gmail.com>
2022-03-10 21:02:23 -0300

    Actualizacion

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ let contador;
 let resultado;
 let productoAComprar;
 let cantidadAComprar;
-let btnComprar = document.getElementById('btnComprar');
+
+let btnComprarSuplementos = document.getElementById('btnComprarSuplementos');
+let btnComprarEquipo = document.getElementById('btnComprarEquipo');
+let btnComprarRopa = document.getElementById('btnComprarRopa');
 
 function totalAPagar(unidadesSolicitadas, precio) {
     let resultado = unidadesSolicitadas * precio;
@@ -16,7 +19,7 @@ function iterarProductos (categoria, filtrado) {
     resultado = productos.filter( (producto) => producto.categoria.includes(filtrado));
     for (const producto of resultado) {
         html += `<li><p>${producto.nombre} $${producto.precio}</li>\n`;
-        document.getElementById('contenedor').innerHTML = html;
+        document.getElementById('lista').innerHTML = html;
     }
 }
 
@@ -29,45 +32,29 @@ function mostrarTotal (productoA, productoB, productoC) {
     } else if (productoAComprar == 2) {
         alert(totalAPagar(productoB.precio, cantidadAComprar));
     } else if (productoAComprar == 3) {
-        alert(totalAPagar(productoC.precio, cantidadAComprar))
+        alert(totalAPagar(productoC.precio, cantidadAComprar));
     }
 }
 
-function mostrarProductos(categoria) {
-    switch (categoria) {
-        case 1:
-            iterarProductos('Suplementos', 'suplementos');
-            setTimeout(mostrarTotal, 1000, producto1, producto2, producto3);
-            break;
-        
-        case 2:
-            iterarProductos('Equipo de gimnasio', 'equipo-gimnasio');
-            setTimeout(mostrarTotal, 1000, producto4, producto5, producto6);
-            break;
-
-        case 3:
-            iterarProductos('Ropa deportiva', 'indumentaria');
-            setTimeout(mostrarTotal, 1000, producto7, producto8, producto9);
-            break;
-
-        default:
-            alert('Error!');
-        break;
-    }
+function mostrarSuplementos () {
+    iterarProductos('Suplementos', 'suplementos');
+    // setTimeout(mostrarTotal, 1000, producto1, producto2, producto3);
 }
 
-let alertas = ['Hola! Bienvenido a la tienda Matfit', 'Somos una tienda en línea de suplementos, equipo de gimnasio y ropa deportiva', '¿Qué productos está buscando?'];
+function mostrarEquipo () {
+    iterarProductos('Equipo de gimnasio', 'equipo-gimnasio');
+    // setTimeout(mostrarTotal, 1000, producto4, producto5, producto6);
+}
+
+function mostrarRopa () {
+    iterarProductos('Ropa deportiva', 'indumentaria');
+    // setTimeout(mostrarTotal, 1000, producto7, producto8, producto9);
+}
+let alertas = ['Hola! Bienvenido a la tienda Matfit', 'Somos una tienda en línea de suplementos, equipo de gimnasio y ropa deportiva', '¡Gracias por elegirnos!'];
 for (alerta of alertas) {
     alert(alerta);
 }
 
-function realizarCompra () {
-    let productoBuscado = parseInt(prompt('Categorias: \n1. Suplementos.\n2. Equipo de gimnasio.\n3. Ropa deportiva.'));
-    if ((productoBuscado != '') && (productoBuscado > 0) && (productoBuscado < 4)) {
-        mostrarProductos(productoBuscado);
-    } else {
-        alert('Error! Ingrese un número valido.');
-    }
-}
-
-btnComprar.addEventListener('click', realizarCompra);
+btnComprarSuplementos.addEventListener('click', mostrarSuplementos);
+btnComprarEquipo.addEventListener('click', mostrarEquipo);
+btnComprarRopa.addEventListener('click', mostrarRopa);
